5fa4b208b470bb65276dc08982e4fb2b9dd51cf3
Dettwiller <idd0790@gmail.com>
2019-08-17 22:35:01 -0500

    Update validation.py

--- validation.py
+++ validation.py
@@ -6,3 +6,8 @@ if __name__ == '__main__':
     data_dir = join(getcwd(), "data")
     model_timeline = [2008, 2018]
     data_timeline = [2005, 2018]
+
+    # TODO use multiprocessing to download all team game data in parallel
+    # TODO use multiprocessing to analyze all games over  model_timeline in parallel
+    # TODO calculate useful values like RMSE
+    # TODO output results to csv
