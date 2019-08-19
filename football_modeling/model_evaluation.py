142bf4ad15ed2f9a7e16c399c0a4682fb4d84120
Dettwiller <idd0790@gmail.com>
2019-08-19 18:37:38 -0500

    actually added model_evaluation

---
+++ football_modeling/model_evaluation.py
@@ -0,0 +1,113 @@
+from os import getcwd
+from os.path import join
+from datetime import datetime
+from csv import reader
+from collections import namedtuple
+
+from football_modeling import tools, model, team, fetch_data
+
+def evaluator(data_dir=getcwd(), games_dir=getcwd(), modeling_data_dir=getcwd()):
+    evaluator_obj = Evaluator(data_dir=data_dir, games_dir=games_dir, modeling_data_dir=modeling_data_dir)
+    return evaluator_obj
+
+class Evaluator:
+    def __init__(self, data_dir=getcwd(), games_dir=getcwd(), modeling_data_dir=getcwd()):
+        tools.directory_check(data_dir)
+        tools.directory_check(games_dir)
+        tools.directory_check(modeling_data_dir)
+        data_dir_check = data_dir in [games_dir, modeling_data_dir]
+        games_dir_check = games_dir in [data_dir, modeling_data_dir]
+        modeling_data_dir_check = modeling_data_dir in [data_dir, games_dir]
+        if data_dir_check or games_dir_check or modeling_data_dir_check:
+            raise Warning("data_dir, games_dir, and modeing_data_dir are not unique.")
+
+        self.data_dir = data_dir
+        self.games_dir = games_dir
+        self.modeling_data_dir = modeling_data_dir
+        self.data_downloader = fetch_data.data_downloader(data_dir=data_dir)
+
+        self.model_data_type = ""
+
+    def __check_data_type(self, data_type):
+        data_types_type_check = isinstance(data_type, str)
+        assert data_types_type_check, "data_type is not string: %r" % type(data_type)
+        data_types_value_check = data_type in self.data_downloader.acceptable_data_types
+        assert data_types_value_check, "data_type (" + data_type + ") must be one of: %r" % self.data_downloader.acceptable_data_types
+        if not self.model_data_type:
+            self.model_data_type = data_type
+        else:
+            model_data_check = self.model_data_type == data_type
+            assert model_data_check, "provided data_type does not match current model_data_type"
+
+    def __get_full_game_list(self, team_names, model_timeline):
+        desired_data = ["id", "away_team", "away_points", "home_team", "home_points", "neutral_site", "season"]
+        Game = namedtuple('Game', 'id away_team away_points home_team home_points neutral_size season')
+        full_game_list = []
+        for team_name in team_names:
+            csv_filename = team_name + "_games_data_" + str(model_timeline[0]) + "-" + str(model_timeline[1]) + ".csv"
+            csv_file = join(self.games_dir, csv_filename)
+            with open(csv_file, "r", encoding='utf-8', errors='ignore') as csvf:
+                game_reader = reader(csvf, delimiter=',', quotechar='"')
+                header_list = next(game_reader)
+                desired_indices = [header_list.index(val) for val in desired_data]
+                away_conference_i = header_list.index('away_conference')
+                home_conference_i = header_list.index('home_conference')
+                for game in game_reader:
+                    if game[away_conference_i] is not None and game[home_conference_i] is not None:
+                        temp_game_list = [game[val] for val in desired_indices]
+                        temp_game_tuple = Game(*temp_game_list)
+                        full_game_list += [temp_game_tuple]
+
+        return full_game_list
+
+
+    def download_game_data(self, model_timeline, print_progress=False):
+        tools.timeline_check(model_timeline)
+        print_progress_type_check = isinstance(print_progress, bool)
+        assert print_progress_type_check, "print_progress is not bool: %r" % type(print_progress)
+
+        self.data_downloader.change_download_directory(self.games_dir)
+        self.data_downloader.parallel_download_all_team_data(data_type='games', timeline=model_timeline, print_progress=print_progress)
+
+    def download_model_data(self, data_timeline, data_type='drives', print_progress=False):
+        self.__check_data_type(data_type)
+        tools.timeline_check(data_timeline)
+        print_progress_type_check = isinstance(print_progress, bool)
+        assert print_progress_type_check, "print_progress is not bool: %r" % type(print_progress)
+
+        self.data_downloader.change_download_directory(self.modeling_data_dir)
+        self.data_downloader.parallel_download_all_team_data(data_type=self.model_data_type, timeline=data_timeline, print_progress=True)
+
+    def evaluate_model(self, eval_model, model_timeline, data_timeline, data_type='drives', print_progress=False):
+        model_type_check = isinstance(eval_model, model.Model)
+        assert model_type_check, "model is not valid instance of model.Model"
+        tools.timeline_check(model_timeline)
+        tools.timeline_check(data_timeline)
+        self.__check_data_type(data_type)
+        print_progress_type_check = isinstance(print_progress, bool)
+        assert print_progress_type_check, "print_progress is not bool: %r" % type(print_progress)
+
+        fbs_teams_df = self.data_downloader.get_all_fbs_teams()
+        fbs_teams_list = fbs_teams_df['school'].tolist()
+
+        if self.model_data_type == 'drives':
+            drives_dir = self.modeling_data_dir
+        else:
+            drives_dir = getcwd()
+
+        # teams_dict = {}
+        # for fbs_team in fbs_teams_list:
+        #     teams_dict[fbs_team] = team.team(fbs_team, data_dir=self.data_dir, games_dir=self.games_dir, drives_dir=drives_dir)
+        #     teams_dict[fbs_team].get_data(data_timeline, data_type='drives', print_progress=print_progress)
+        #     teams_dict[fbs_team].get_data(model_timeline, data_type='games', print_progress=print_progress)
+
+        # ("id", "away_team", "away_points", "home_team", "home_points", "neutral_site", "season")
+        full_game_list = self.__get_full_game_list(fbs_teams_list, model_timeline)
+        
+        # TODO create a function to take a tuple from full_game_list and the teams (from team_dict) 
+        # TODO and predict each game, then analyze the prediction
+        # TODO parallelize that analysis
+
+        # TODO calculate useful values like RMSE
+        # TODO output results to csv
+ 
